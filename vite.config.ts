017a5dad282094ae79086f2eb66d3e027b56ef46
Reina <two.oao@gmail.com>
2025-12-18 15:20:12 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,14 @@
-import { fileURLToPath, resolve, URL } from 'node:url'
-
+import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+  base: '/ts-final-front/', // ← 換成你的 repo 名稱
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  build: {
-    target: 'esnext',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-      output: {
-        dir: resolve(__dirname, 'dist'),
-      },
     },
   },
   css: {
